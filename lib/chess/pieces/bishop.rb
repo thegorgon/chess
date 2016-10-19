39fe89001e40af11106d721b003d4d03bba3ed3c
Jesse Reiss <jessereiss@squareup.com>
2016-10-18 23:24:08 -0700

    Generalize directional moves

--- lib/chess/pieces/bishop.rb
+++ lib/chess/pieces/bishop.rb
@@ -1,80 +1,14 @@
 module Chess
   module Piece
     class Bishop < Base
+      include Helper::Directions
+
       def valid_moves
-        valid_moves_nw +
-          valid_moves_ne +
-          valid_moves_sw +
-          valid_moves_se
+        valid_moves_heading(:northwest) +
+          valid_moves_heading(:northeast) +
+          valid_moves_heading(:southwest) +
+          valid_moves_heading(:southeast)
       end
-
-      # Explore NW diagonal
-      def valid_moves_nw
-        results = []
-        rowptr = @position.row
-        colptr = @position.col
-        while rowptr < @board.height - 1 && colptr > 0
-          rowptr += 1
-          colptr -= 1
-          other = @board.fetch([rowptr, colptr]).piece
-          break if other && other.color == @color
-          results << Position.normalize([rowptr, colptr])
-          break if other && other.color != @color
-        end
-        results
-      end
-      private :valid_moves_nw
-
-      # Explore NE diagonal
-      def valid_moves_ne
-        results = []
-        rowptr = @position.row
-        colptr = @position.col
-        while rowptr < @board.height - 1 && colptr < @board.width - 1
-          rowptr += 1
-          colptr += 1
-          other = @board.fetch([rowptr, colptr]).piece
-          break if other && other.color == @color
-          results << Position.normalize([rowptr, colptr])
-          break if other && other.color != @color
-        end
-        results
-      end
-      private :valid_moves_ne
-
-      # Explore SW diagonal
-      def valid_moves_sw
-        results = []
-        rowptr = @position.row
-        colptr = @position.col
-        while rowptr > 0 && colptr > 0
-          rowptr -= 1
-          colptr -= 1
-          other = @board.fetch([rowptr, colptr]).piece
-          break if other && other.color == @color
-          results << Position.normalize([rowptr, colptr])
-          break if other && other.color != @color
-        end
-        results
-      end
-      private :valid_moves_sw
-
-      # Explore SE diagonal
-      def valid_moves_se
-        results = []
-        rowptr = @position.row
-        colptr = @position.col
-        while rowptr > 0 && colptr < @board.width - 1
-          rowptr -= 1
-          colptr += 1
-          other = @board.fetch([rowptr, colptr]).piece
-          break if other && other.color == @color
-          results << Position.normalize([rowptr, colptr])
-          break if other && other.color != @color
-        end
-        results
-      end
-      private :valid_moves_se
     end
   end
 end
---
+++ lib/chess/pieces/helpers/directions.rb
@@ -0,0 +1,71 @@
+module Chess
+  module Piece
+    module Helper
+      module Directions
+        def directional_details(board)
+          {
+            north: {
+              iteration: { row: 1, col: 0 },
+              bounds: { row: { lt: @board.height - 1 } }
+            },
+            northeast: {
+              iteration: { row: 1, col: 1 },
+              bounds: { row: { lt: @board.height - 1 }, col: { lt: @board.width - 1 } }
+            },
+            east: {
+              iteration: { row: 0, col: 1 },
+              bounds: { col: { lt: @board.width - 1 } }
+            },
+            southeast: {
+              iteration: { row: -1, col: 1 },
+              bounds: { row: { gt: 0 }, col: { lt: @board.width - 1 } }
+            },
+            south: {
+              iteration: { row: -1, col: 0 },
+              bounds: { row: { gt: 0 } }
+            },
+            southwest: {
+              iteration: { row: -1, col: -1 },
+              bounds: { row: { gt: 0 }, col: { gt: 0 } }
+            },
+            west: {
+              iteration: { row: 0, col: -1 },
+              bounds: { col: { gt: 0 } }
+            },
+            northwest: {
+              iteration: { row: 1, col: -1 },
+              bounds: { row: { lt: @board.height - 1 }, col: { gt: 0 } }
+            }
+          }
+        end
+        private :directional_details
+
+        def in_bounds?(row, col, bounds)
+          (bounds[:row].nil? || (bounds[:row][:gt] && row > bounds[:row][:gt]) || (bounds[:row][:lt] && row < bounds[:row][:lt])) &&
+            (bounds[:col].nil? || (bounds[:col][:gt] && col > bounds[:col][:gt]) || (bounds[:col][:lt] && col < bounds[:col][:lt]))
+        end
+        private :in_bounds?
+
+        def valid_moves_heading(direction)
+          details = directional_details(@board)[direction]
+          raise ArgumentError, "Invalid direction: '#{direction}'" unless details
+
+          results = []
+          rowptr = @position.row
+          colptr = @position.col
+          while in_bounds?(rowptr, colptr, details[:bounds])
+            rowptr += details[:iteration][:row]
+            colptr += details[:iteration][:col]
+            curpos = Position.normalize([rowptr, colptr])
+            other = @board.fetch(curpos).piece
+            break if other && other.color == @color
+            results << curpos
+            break if other && other.color != @color
+          end
+          results
+        end
+        private :valid_moves_heading
+      end
+    end
+  end
+end
